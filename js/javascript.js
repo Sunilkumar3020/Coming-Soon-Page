c255c847f02240f7ee4ee536e87736d2e7fa37ea
Sunil kumar <sunilkumar3020@gmail.com>
2019-02-14 15:44:45 +0530

    complete coming soon page

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+"use strict";
+var countDownDate = new Date("April 1, 2019 00:00:00").getTime();
+
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    var seconds = Math.floor(distance % (1000 * 60) / 1000);
+    
+    document.getElementById("launch").innerHTML = days + "d  " + hours + "h  " + minutes + "m  " + seconds + "s  ";
+    
+    if ( distance < 0){
+        clearInterval(x);
+        document.getElementById("launch").innerHTML = "EXPIRED";
+    }
+}, 1000);
